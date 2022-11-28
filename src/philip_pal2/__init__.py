084954749b4434605c4d9b213c3453d02b3413fd
MrKevinWeiss <weiss.kevin604@gmail.com>
2022-11-28 17:05:36 +0100

    feat: Initial import

--- src/philip_pal2/__init__.py
+++ src/philip_pal2/__init__.py
@@ -1,5 +1,10 @@
 import sys
 
+from philip_pal2.philip_if import Philip as Phil
+
+__all__ = ["Phil"]
+
+
 if sys.version_info[:2] >= (3, 8):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
     from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
